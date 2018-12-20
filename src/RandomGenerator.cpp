6f57372187ee57a2b26c1431a9e8b7136bbdc62f
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2018-12-20 15:24:56 +0100

    Create RandomGenerator.cpp

---
+++ src/RandomGenerator.cpp
@@ -0,0 +1,19 @@
+#include "RandomGenerator.h"
+
+#ifdef _WIN32 || _WIN64
+
+#include <windows.h>
+#include <intrin.h>
+
+double RandomGen(int start, int end) {
+	unsigned __int64 i = __rdtsc();
+	i <<= 54;
+	double j = (double)(i >> 54)/999.00;
+	return (double)start + (((double)end - (double)start) * (j > 1.0 ? 1.0 : j));
+}
+
+#else
+
+double RandomGen(int start, int end) {}
+
+#endif // WIN32 || WIN64
